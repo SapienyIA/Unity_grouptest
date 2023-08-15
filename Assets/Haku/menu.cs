879963c53d24613ae5693b62d4c676a81daf1f4a
0857xman <138540069+0857xman@users.noreply.github.com>
2023-08-15 13:22:17 +0800

    Update Sound

--- Assets/Haku/menu.cs
+++ Assets/Haku/menu.cs
@@ -4,6 +4,6 @@ namespace Haku
 {
     public class menu : MonoBehaviour
     {
-
+        bool start = false;
     }
 }
---
+++ Assets/XMAN/Sound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+
+namespace XMAN
+{
+    public class Sound : MonoBehaviour
+    {
+
+    }
+}
+
